8aed9a8ac0fc43c7b4a8ca9d8c74e2008f9cf204
Ari Berkowicz <berkowicz.ari@gmail.com>
2021-04-21 21:06:17 -0700

    Fixed the formatting and visual

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,36 +6,9 @@ export default function NavBar() {
     const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
     return (
       <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-        <Navbar bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand href="https://www.knowledgefactory.net/">
-                                       Knowledgefactory</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="https://www.knowledgefactory.net/">Home
-                                                        </Nav.Link>
-                        <Nav.Link href="https://www.knowledgefactory.net
-                           /p/knowledefactory-topics.html">SpringBoot</Nav.Link>
-                        <NavDropdown title="Web Development" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="https://www.knowledgefactory.net
-                              /p/reactjs-tutorial.html">ReactJS</NavDropdown.Item>
-                            <NavDropdown.Item href="https://www.knowledgefactory.net
-                             /p/react-native.html">React Native</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="https://www.knowledgefactory.net
-                              /p/node-js.html">NodeJS</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className=
-                                                        "mr-sm-2" />
-                        <Button variant="outline-primary">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
 
         <Navbar collapseOnSelect fixed='top' expand = 'sm' bg='pink' variant = 'purple'>
-                <Container>
+                {/* <Container>
                     <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
                     <Navbar.Collapse id='responsive-navbar-nav'/>
                     <Nav>
@@ -44,7 +17,30 @@ export default function NavBar() {
                         <Nav.Link href='/'>STORIES</Nav.Link>
                         <Nav.Link href='/'>MEDIA</Nav.Link>
                     </Nav>
-                </Container>
+                </Container> */}
+
+                <Nav.Item>
+          <Nav.Link eventKey="1" href="#/home">
+            NavLink 1 content
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="2" title="Item">
+            NavLink 2 content
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="3" disabled>
+            NavLink 3 content
+          </Nav.Link>
+        </Nav.Item>
+        <NavDropdown title="Dropdown" id="nav-dropdown">
+          <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
             </Navbar>
       </Nav>
     )
