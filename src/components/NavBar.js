568359dd4ce3c49fe574e682aed9b57c2e0fc55d
Ari Berkowicz <berkowicz.ari@gmail.com>
2021-04-21 21:00:43 -0700

    Created a stable version of a function version of the navbar to make everything unified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,35 @@
 import React,{Component} from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
 
-export default class NavBar extends Component{
-    render(){
-        return (
-        <div>
-            <Navbar collapseOnSelect fixed='top' expand = 'sm' bg='pink' variant = 'purple'>
+export default function NavBar() {
+    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+    return (
+      <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+        <Navbar collapseOnSelect fixed='top' expand = 'sm' bg='pink' variant = 'purple'>
                 <Container>
                     <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
                     <Navbar.Collapse id='responsive-navbar-nav'/>
@@ -17,10 +41,6 @@ export default class NavBar extends Component{
                     </Nav>
                 </Container>
             </Navbar>
-        </div>
+      </Nav>
     )
-    }
-    
-};
-
-
+  }
