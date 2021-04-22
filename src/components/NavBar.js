c45d8cd3981d9dd2e327d7167014f999c07e166f
Ari Berkowicz <berkowicz.ari@gmail.com>
2021-04-21 21:04:22 -0700

    Fixed the formating and now everything is actually visual

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,38 @@
 import React,{Component} from 'react';
-import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown,  Form, Button, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NavBar() {
     const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
     return (
       <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+        <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand href="https://www.knowledgefactory.net/">
+                                       Knowledgefactory</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="https://www.knowledgefactory.net/">Home
+                                                        </Nav.Link>
+                        <Nav.Link href="https://www.knowledgefactory.net
+                           /p/knowledefactory-topics.html">SpringBoot</Nav.Link>
+                        <NavDropdown title="Web Development" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="https://www.knowledgefactory.net
+                              /p/reactjs-tutorial.html">ReactJS</NavDropdown.Item>
+                            <NavDropdown.Item href="https://www.knowledgefactory.net
+                             /p/react-native.html">React Native</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="https://www.knowledgefactory.net
+                              /p/node-js.html">NodeJS</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className=
+                                                        "mr-sm-2" />
+                        <Button variant="outline-primary">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
 
         <Navbar collapseOnSelect fixed='top' expand = 'sm' bg='pink' variant = 'purple'>
                 <Container>
