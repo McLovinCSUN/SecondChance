09ccfd792bc29c5c11081590e9cad3ebbe775b87
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2021-04-30 17:52:20 -0700

    added what content should be in homepage. still need to work on responsivness. footer looks good for a tablet and desktop.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
-import React,{useRef, useState} from 'react';
+import React,{useState} from 'react';
 import './Footer.css';
+import { FaMapMarkerAlt,  FaEnvelope} from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export default function Footer() {
     const [copySuccess, setCopySuccess] = useState('');
@@ -10,21 +12,26 @@ export default function Footer() {
     setCopySuccess('Copied!');
   };
 
-
     return (
         <div className='footerContainer'>
-            <div className='address'>Project Rebound<br/>CHS JR Hall 148<br/>18111 Nordhoff Street<br/>Northridge, CA<br/> 91330-8246</div>     
+            <div className='address'>
+              <IconContext.Provider value={{ color: "#fe7ec9", className: "map" }}>
+                <FaMapMarkerAlt/> 
+              </IconContext.Provider>
+              <br/>
+              <p>Project Rebound<br/>CHS JR Hall 148<br/>18111 Nordhoff Street<br/>Northridge, CA<br/> 91330-8246</p> 
+            </div>     
             <div className='email'>
-       
- <button 
-  onClick={copyToClipboard}
->
- projectrebound@csun.edu
-</button>
-{copySuccess}
-      </div>
-      </div> 
-        
+              <IconContext.Provider value={{ color: "#38c4c1", className: "envelope" }}>
+                <FaEnvelope/>
+              </IconContext.Provider>
+              <br/>
+              <button onClick={copyToClipboard}>
+                projectrebound@csun.edu
+              </button>
+              {copySuccess}
+            </div>
+          </div> 
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import './Home.css';
+import GuyCoding from '../images/prisoners-tech.png';
 
 export default function Home() {
     return (
-        <>
+      <>
         <div className='homeContainer'>
-            <div className='boxOne'><img src="https://betheknockout.com/wp-content/uploads/2020/08/dwayne-johnson-wrestling-debut.jpg" alt="Boomer doggo."/>
-          <p>The star knew at a very young age that hard work is the only way to secure his future, so when his NFL dream was shattered, he quickly turned his focus to professional wrestling.
-            After numerous tryout matches, Dwayne got his first WWE debut (also known as WWF) under the ring name of “Rocky Maivia”. He still looks quite chubby here, don’t you think?</p>
-
+          <div className='boxOne'>
+            <h1 className='headerHomePage'>
+              EVER CONSIDERED LEARNING TO CODE AS A FORM TO REDUCE THE RECIDIVISM RATE IN UNITED STATES?
+            </h1>  
           </div>
-            <div className='boxTwo'><img src="https://betheknockout.com/wp-content/uploads/2020/08/dwayne-johnson-wrestling-debut.jpg" alt="Boomer doggo."/>
-          <p>The star knew at a very young age that hard work is the only way to secure his future, so when his NFL dream was shattered, he quickly turned his focus to professional wrestling.
-            After numerous tryout matches, Dwayne got his first WWE debut (also known as WWF) under the ring name of “Rocky Maivia”. He still looks quite chubby here, don’t you think?</p>
-        </div>
-        </div>
-        <div className='homeContainer'>
-            <div className='boxThree'><img src="https://betheknockout.com/wp-content/uploads/2020/08/dwayne-johnson-wrestling-debut.jpg" alt="Boomer doggo."/>
-      </div>
-            <div className='boxFour'><img src="https://betheknockout.com/wp-content/uploads/2020/08/dwayne-johnson-wrestling-debut.jpg" alt="Boomer doggo."/></div>
-            <div className='boxFive'><img src="https://betheknockout.com/wp-content/uploads/2020/08/dwayne-johnson-wrestling-debut.jpg" alt="Boomer doggo."/></div>
-            <div className='boxSix'><img src="https://betheknockout.com/wp-content/uploads/2020/08/dwayne-johnson-wrestling-debut.jpg" alt="Boomer doggo."/></div>
-           
+          <div className='boxTwo'>             
+            <img className='techGuy' src={GuyCoding} alt="Boomer doggo."/>
+          </div>
         </div>
         <div className='homeContainer'>
-         <div className='boxSeven'><h1>About
-Second Chance Through Code</h1>
-<h3>This site will list free resources where formerly incarcerated people can find organizations that offer free mentorship and/or classes to learn a career skill in coding. The main goal that we are trying to accomplish is listing all these valuable resources in one central location to have them accessible for those who are re-entering society and ready to start their second chance in life. </h3>
-</div>
-         </div>
-        </>
+          <div className='boxThree'><img src="https://www.comevo.com/wp-content/uploads/2019/10/Blog-post-photo.jpg" alt="Boomer doggo."/></div>
+          <div className='boxFour'><img src="https://worktogether4peace.org/wp-content/uploads/2018/04/29541523_10155177571686237_7583812550276138378_n.jpg" alt="Boomer doggo."/></div>
+          <div className='boxFive'><img src="https://images.theconversation.com/files/378668/original/file-20210113-23-1trii4t.png?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=321&fit=crop&dpr=1" alt="Boomer doggo."/></div>
+          <div className='boxSix'><img src="https://i.pinimg.com/originals/78/f7/71/78f771e3daeb0da4886230e9403a0925.png" alt="Boomer doggo."/></div>    
+        </div>
+        <div className='homeContainer'>
+          <div className='boxSeven'>
+            <h3 className='headerHomePage'>About Second Chance Through Code</h3>
+            <hr className="rounded"></hr>
+            <h4>This site will list free resources where formerly incarcerated people can find organizations that offer free mentorship and/or classes to learn a career skill in coding. The main goal that we are trying to accomplish is listing all these valuable resources in one central location to have them accessible for those who are re-entering society and ready to start their second chance in life. </h4>
+          </div>
+        </div>
+      </>
     )
 }
