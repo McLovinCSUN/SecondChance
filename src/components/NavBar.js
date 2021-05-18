38f840a21b2d4512050f995962d66d514e75982c
jonsals <jonathan.salcedomanriquez.934@my.csun.edu>
2021-05-18 01:44:14 -0700

    Change navigation bar
    the navigation bar now switches to a button that will drop down once click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, Nav, NavDropdown,  Form, Button, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavBar.css';
+import './buttonToggle.js';
 
 export default function NavBar() {
     const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
@@ -9,29 +10,27 @@ export default function NavBar() {
       <Nav variant="pills" activeKey="1" onSelect={handleSelect} >
         
         <Navbar className="navigationBar" variant="light" collapseOnSelect fixed='top' expand='xl' >
+        <script type = "buttonToggle.js"></script>
                  <h4>SECOND CHANCE THROUGH CODE</h4>
-                        <Nav.Link href="/home">HOME</Nav.Link>
-                        <Nav.Link href='/resources'>RESOURCES</Nav.Link>
-                        <Nav.Link href='/stories'>STORIES</Nav.Link>
-                        <Nav.Link href='/media'>MEDIA</Nav.Link>
-
-  
-            
-        {/* <NavDropdown title="Dropdown" id="nav-dropdown">
-          <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-        </NavDropdown> */}
-        <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" bg='light'/>
-      <Button variant="outline-light">Search</Button>
-    </Form>
+                 <a href ='#' class = "toggle-button">
+                    <span class ="bar"></span>
+                    <span class ="bar"></span>
+                    <span class ="bar"></span>
+                  </a>
+                 <div class="navbar-link">
+                   <ul>
+                     <li><a href ="#">HOMES</a></li>
+                     <li><a href ="#">RESOURCES</a></li>
+                     <li><a href ="#">STORIES</a></li>
+                     <li><a href ="#">MEDIA</a></li>
+                  </ul>
+                 </div>
             </Navbar>
         
       </Nav>
     )
   }
+
+
 
 
---
+++ src/components/buttonToggle.js
@@ -0,0 +1,3 @@
+let toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-link')[0]
+
