f1b8185d7761ac64aa055cd630244afbe601a476
Ari Berkowicz <berkowicz.ari@gmail.com>
2021-05-19 18:33:33 -0700

    Fixed most of the responsiveness for the front page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import './Home.css';
 import GuyCoding from '../images/prisoners-tech.png';
+import {Container, Row, Col } from 'react-bootstrap';
 
 export default function Home() {
     return (
       <>
+      <Container>
+      <Container>
         <div className='homeContainer'>
+        <Row>
+          <Col>
           <div className='boxOne'>
             <h1 className='headerHomePage'>
               EVER CONSIDERED LEARNING TO CODE AS A FORM TO REDUCE THE RECIDIVISM RATE IN UNITED STATES?
             </h1>  
           </div>
+          </Col>
+          <Col>
           <div className='boxTwo'>             
             <img className='techGuy' src={GuyCoding} alt="Inmate coding on a desktop"/>
           </div>
+        </Col>
+          </Row>
         </div>
-        <div className='homeContainer-2'>
+        </Container>
+        <Container>
+        <Row>
+          <Col>
           <div className='boxThree'><img src="https://www.comevo.com/wp-content/uploads/2019/10/Blog-post-photo.jpg" alt="chain and ball with a graduation hat"/></div>
+          </Col>
+          <Col>
           <div className='boxFour'><img src="https://worktogether4peace.org/wp-content/uploads/2018/04/29541523_10155177571686237_7583812550276138378_n.jpg" alt="Incarceration VS Education Image"/></div>
+          </Col>
+          <Col>
           <div className='boxFive'><img src="https://images.theconversation.com/files/378668/original/file-20210113-23-1trii4t.png?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=321&fit=crop&dpr=1" alt="Stating figures on incarceration"/></div>
+          </Col>
+          <Col>
           <div className='boxSix'><img src="https://i.pinimg.com/originals/78/f7/71/78f771e3daeb0da4886230e9403a0925.png" alt="A statement on re-entry programs giving opportunities to returning citizens"/></div>    
-        </div>
+          </Col>
+        </Row>
+        </Container>
+        <Container>
         <div className='homeContainer'>
           <div className='boxSeven'>
             <h3 className='headerHomePage'>About Second Chance Through Code</h3>
@@ -28,6 +49,8 @@ export default function Home() {
             <h4>This site will list free resources where formerly incarcerated people can find organizations that offer free mentorship and/or classes to learn a career skill in coding. The main goal that we are trying to accomplish is listing all these valuable resources in one central location to have them accessible for those who are re-entering society and ready to start their second chance in life. </h4>
           </div>
         </div>
+        </Container>
+        </Container>
       </>
     )
 }
